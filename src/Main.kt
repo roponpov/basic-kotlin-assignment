c4ec6653e07e198cd575e117c26a54e7f7fa4297
Pov Ropon <19993@kh2-acledabank.local>
2025-11-13 16:15:09 +0700

    prepared main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -7,38 +7,46 @@ fun main() {
     val exerciseTwo: ExerciseTwo = ExerciseTwo()
     val exerciseThree: ExerciseThree = ExerciseThree()
 
+
+
+
     ////////////// CALL METHOD EXERCISE ONE //////////////
-//    exerciseOne.createVariableUsingVarAndVal()
+    exerciseOne.createVariableUsingVarAndVal()
+
+
+
 
     ////////////// CALL METHOD EXERCISE TWO //////////////
+    //exerciseTwo.guestTheNumber(randomMaximum = 10)
 
-        exerciseTwo.guestTheNumber(randomMaximum = 10)
+    //exerciseTwo.findMaximumNumber(x = 369,y= 963)
 
-        exerciseTwo.findMaximumNumber(x = 369,y= 963)
+    //val result = exerciseTwo.transformString(
+    //    text = "Good Morning",
+    //    operation = {
+    //        it.lowercase()
+    //    }
+    //)
+    //println(result)
 
-        val result = exerciseTwo.transformString(
-            text = "Good Morning",
-            operation = {
-                it.lowercase()
-            }
-        )
-        println(result)
+    //exerciseTwo.loadDataFromRemote { places ->
+    //    places.map { place ->
+    //        println(place.toString())
+    //    }
+    //}
 
-        exerciseTwo.loadDataFromRemote { places ->
-            places.map { place ->
-                println(place.toString())
-            }
-        }
+    //val reversedText = exerciseTwo.reverseString("ROPON")
+    //println(reversedText)
 
-        val reversedText = exerciseTwo.reverseString("ROPON")
-        println(reversedText)
+    //val luckyNumber :Int = exerciseTwo.calculateTotalSumNumber(arrayListOf(12,13,14)) // 39
+    //println(luckyNumber)
 
-        val luckyNumber :Int = exerciseTwo.calculateTotalSumNumber(arrayListOf(12,13,14)) // 39
-        println(luckyNumber)
+    //val isSingle: Boolean = exerciseTwo.checkBirthdayIsSingle(12,9,2002)
+    //if (isSingle) println("You are single.") else println("You’re taken.")
 
-        val isSingle: Boolean = exerciseTwo.checkBirthdayIsSingle(12,9,2002)
-        if (isSingle) println("You are single.") else println("You’re taken.")
+
+
 
     ////////////// CALL METHOD EXERCISE THREE //////////////
-        exerciseThree.main()
+    //exerciseThree.main()
 }
